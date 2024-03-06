9b5a6350c60e1292d1fdbff904e381b862c373ea
evan-newman <newmanrevan@gmail.com>
2024-03-06 14:16:23 -0700

    updating the modules package tried npm install in vexflow directory don't think it will help.

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-import { Vex } from "https://cdn.jsdelivr.net/npm/vexflow@4.2.2/build/cjs/vexflow.js";
+import { Vex } from "vexflow";
 const { Factory, EasyScore, System } = Vex.Flow;
 
 const vf = new Factory({
