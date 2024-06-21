f51bf752d2c95f683ab6f27e0c7ea37f5d1b89a8
evan-newman <newmanrevan@gmail.com>
2024-06-21 17:47:02 -0600

    Successfully added working code that is able to draw a stave with a whole note that adjusts it's x position to sit in the middle of the stave. Also have logic that is able to remove the notes on the stave in order to be able to draw new notes. This sets the initial logic up in order to automate this process when a user enters the correct chord to then wipe the stave of the old chord to draw a new chord on the stave.

--- play.js
+++ play.js
@@ -1,21 +1,104 @@
-import { Vex } from "./vexflow/build/cjs/vexflow.js";
-const { Factory, EasyScore, System } = Vex.Flow;
+const { 
+  Renderer, 
+  Stave, 
+  StaveNote, 
+  Voice, 
+  Formatter,
+  TickContext,
+} = Vex.Flow;
 
-const vf = new Factory({
-  renderer: { elementId: 'output', width: 500, height: 200 },
+const div = document.getElementById("output");
+const renderer = new Renderer(div, Renderer.Backends.SVG);
+
+renderer.resize(225, 200);
+const context = renderer.getContext();
+
+const stave = new Stave(10, 40, 200);
+stave.addClef("treble");
+
+stave.setContext(context).draw();
+
+const notes = [
+  new StaveNote({
+    keys: ['c/4'],
+    duration: 'w'
+  })
+];
+
+const voices = [
+  new Voice({
+    num_beats: 4,
+    beat_value: 4
+  }).addTickables(notes)
+]
+
+new Formatter().joinVoices(voices).format(voices, 100);
+
+notes[0].getTickContext().setX(50);
+
+const group = context.openGroup();
+
+voices.forEach(function(v) {
+  v.draw(context, stave);
 });
+
+context.closeGroup();
+context.svg.removeChild(group);
+
+/*
+// const vf = new Factory({
+//     renderer: { elementId: 'output', width: 225, height: 200 },
+// });
 
 const score = vf.EasyScore();
 const system = vf.System();
 
-system
-  .addStave({
-    voices: [
-      score.voice(score.notes('C#5/q, B4, A4, G#4', { stem: 'up' })),
-      score.voice(score.notes('C#4/h, C#4', { stem: 'down' })),
-    ],
-  })
-  .addClef('treble')
-  .addTimeSignature('4/4');
+const notes = ['E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5'];
 
-vf.draw();
+function createStaff() {
+  let pickRoot = Math.floor(Math.random() * (notes.length - 4));
+  let constructChord = new String("");
+  for (let i = pickRoot; i <= pickRoot + 4; i += 2) {
+      constructChord += notes[i] + " ";
+  }
+  const fullChord = "(" + constructChord + ")/w";
+  console.log(fullChord);
+
+  system
+  .addStave({
+      voices: [
+          score.voice(score.notes(fullChord))
+      ],
+  })
+  .addClef('treble');
+
+  vf.draw();
+
+  return pickRoot;
+}
+
+let root = createStaff();
+
+const input = document.getElementById('text-box');
+input.focus();
+input.select();
+
+document.addEventListener("click", function() {
+  input.focus();
+});
+
+function handle(e) {
+  if(e.keyCode === 13){
+    console.log(input.value);
+    console.log(notes[root][0])
+    if (input.value === notes[root][0]) {
+      const canvas = document.getElementById('output');
+      const context = canvas.getContext("2d");
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      root = createStaff();
+    } else {
+      console.log('Wrong!')
+    }
+  }
+}
+*/
