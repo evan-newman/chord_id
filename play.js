f12d5b90ce5046d4f508f884e2faf65a20bf3825
evan-newman <newmanrevan@gmail.com>
2024-06-21 18:51:09 -0600

    Removed old uncommented code. Added event listener on keydown so that the input field would be auto focused for user input. In future want to clean up the UI to feel more elegant, remove input field box border, add blinking cursor to idle input, increase size of input field, change positioning of play-page to sit more center of screen vertically

--- play.js
+++ play.js
@@ -58,9 +58,9 @@ const input = document.getElementById('text-box');
 input.focus();
 input.select();
 
-// document.addEventListener("click", function() {
-//   input.focus();
-// });
+document.addEventListener("keydown", function() {
+  input.focus();
+});
 
 function handle(e) {
   if(e.keyCode === 13){
@@ -79,61 +79,3 @@ function handle(e) {
     }
   }
 }
-
-/*
-// const vf = new Factory({
-//     renderer: { elementId: 'output', width: 225, height: 200 },
-// });
-
-const score = vf.EasyScore();
-const system = vf.System();
-
-const notes = ['E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5'];
-
-function createStaff() {
-  let pickRoot = Math.floor(Math.random() * (notes.length - 4));
-  let constructChord = new String("");
-  for (let i = pickRoot; i <= pickRoot + 4; i += 2) {
-      constructChord += notes[i] + " ";
-  }
-  const fullChord = "(" + constructChord + ")/w";
-  console.log(fullChord);
-
-  system
-  .addStave({
-      voices: [
-          score.voice(score.notes(fullChord))
-      ],
-  })
-  .addClef('treble');
-
-  vf.draw();
-
-  return pickRoot;
-}
-
-let root = createStaff();
-
-const input = document.getElementById('text-box');
-input.focus();
-input.select();
-
-document.addEventListener("click", function() {
-  input.focus();
-});
-
-function handle(e) {
-  if(e.keyCode === 13){
-    console.log(input.value);
-    console.log(notes[root][0])
-    if (input.value === notes[root][0]) {
-      const canvas = document.getElementById('output');
-      const context = canvas.getContext("2d");
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      root = createStaff();
-    } else {
-      console.log('Wrong!')
-    }
-  }
-}
-*/
