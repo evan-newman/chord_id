d6e992231643903d3ee4b0bc2b314e509a5574b4
evan-newman <newmanrevan@gmail.com>
2024-06-21 18:27:56 -0600

    Took previous logic and placed drawing the chord into the function drawChord. Added logic so that whenever a user enters the correct chord name in the input field and presses enter the stave will clear the chord and the drawChord function will be called to construct a new chord for the user to guess.

--- play.js
+++ play.js
@@ -4,7 +4,6 @@ const {
   StaveNote, 
   Voice, 
   Formatter,
-  TickContext,
 } = Vex.Flow;
 
 const div = document.getElementById("output");
@@ -18,32 +17,68 @@ stave.addClef("treble");
 
 stave.setContext(context).draw();
 
-const notes = [
-  new StaveNote({
-    keys: ['c/4'],
-    duration: 'w'
-  })
-];
+let group = context.openGroup();
+const pickKey = ['e/3', 'f/3', 'g/3', 'a/3', 'b/3', 'c/4', 'd/4', 'e/4', 'f/4', 'g/4', 'a/4', 'b/4', 'c/5', 'd/5', 'e/5', 'f/5', 'g/5', 'a/5', 'b/5'];
 
-const voices = [
-  new Voice({
-    num_beats: 4,
-    beat_value: 4
-  }).addTickables(notes)
-]
+/******************************************/
 
-new Formatter().joinVoices(voices).format(voices, 100);
+function drawChord() {
+  let pickRoot = Math.floor(Math.random() * (pickKey.length - 4));
+  const chord = [pickKey[pickRoot], pickKey[pickRoot + 2], pickKey[pickRoot + 4]];
 
-notes[0].getTickContext().setX(50);
+  const notes = [
+    new StaveNote({
+      keys: chord,
+      duration: 'w'
+    })
+  ]
 
-const group = context.openGroup();
+  const voices = [
+    new Voice({
+      num_beats: 4,
+      beat_value: 4
+    }).addTickables(notes),
+  ]
 
-voices.forEach(function(v) {
-  v.draw(context, stave);
-});
+  new Formatter().joinVoices(voices).format(voices, 300);
 
-context.closeGroup();
-context.svg.removeChild(group);
+  notes[0].getTickContext().setX(50);
+
+
+  voices.forEach(function(v) {
+    v.draw(context, stave);
+  });
+  return pickKey[pickRoot][0]
+}
+
+/*****************************************/
+let chordRoot = drawChord()
+
+const input = document.getElementById('text-box');
+input.focus();
+input.select();
+
+// document.addEventListener("click", function() {
+//   input.focus();
+// });
+
+function handle(e) {
+  if(e.keyCode === 13){
+    console.log(input.value);
+    console.log(chordRoot)
+    if (input.value.toLowerCase() === chordRoot) {
+      context.closeGroup();
+      context.svg.removeChild(group);
+
+      input.value = "";
+
+      group = context.openGroup();
+      chordRoot = drawChord()
+    } else {
+      console.log('Wrong!')
+    }
+  }
+}
 
 /*
 // const vf = new Factory({
