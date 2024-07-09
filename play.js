5924e161c45668bbd4ce6c0049f14cb69c6007bb
evan-newman <newmanrevan@gmail.com>
2024-07-09 13:57:35 -0700

    fixed chord construction to accurately create a major chord from picking one of the 7 natural keys or white keys as the root. So far program only can construct major chords. Looking to add minor chords next. Afterwards add in settings to select if you want major, minor, or both to test your knowledge

--- play.js
+++ play.js
@@ -4,6 +4,7 @@ const {
   StaveNote, 
   Voice, 
   Formatter,
+  Accidental,
 } = Vex.Flow;
 
 const div = document.getElementById("output");
@@ -14,25 +15,63 @@ const context = renderer.getContext();
 context.setViewBox(45, 10, 110, 110);
 
 const stave = new Stave(0, 0, 200);
-stave.addClef("treble");
+stave.addClef("treble").addKeySignature('C');
 
 stave.setContext(context).draw();
 
 let group = context.openGroup();
+const allKeys = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
 const pickKey = ['e/3', 'f/3', 'g/3', 'a/3', 'b/3', 'c/4', 'd/4', 'e/4', 'f/4', 'g/4', 'a/4', 'b/4', 'c/5', 'd/5', 'e/5', 'f/5', 'g/5', 'a/5', 'b/5'];
+const rootNotes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+let madeChord = constructMajorChord();
+
+function constructMajorChord() {
+  let pickRoot = Math.floor(Math.random() * rootNotes.length);
+  let range = 3;
+  let shift = 3;
+  if (pickRoot <= 1) {
+    range = 2;
+    shift = 4;
+  } else if (pickRoot >= 5) {
+    range = 2;
+    shift = 3;
+  }
+  let pitch = Math.floor(Math.random() * range) + shift;
+  let noteIdx = allKeys.findIndex(x => x == rootNotes[pickRoot]);
+
+  let chord = [];
+  let majorChordAlg = [0, 4, 3];
+  for (const halfSteps of majorChordAlg) {
+    noteIdx += halfSteps;
+    if (noteIdx >= allKeys.length) {
+      noteIdx -= allKeys.length;
+      pitch++;
+    }
+
+    const note = allKeys[noteIdx] + '/' + pitch;
+    chord.push(note);
+  }
+
+  return chord;
+}
 
 /******************************************/
 
 function drawChord() {
-  let pickRoot = Math.floor(Math.random() * (pickKey.length - 4));
-  const chord = [pickKey[pickRoot], pickKey[pickRoot + 2], pickKey[pickRoot + 4]];
+  const chord = constructMajorChord();
+  console.log(chord);
 
-  const notes = [
-    new StaveNote({
-      keys: chord,
-      duration: 'w'
-    })
-  ]
+  let madeStave = new StaveNote({
+    keys: chord,
+    duration: 'w'
+  })
+  for (let i = 0; i < chord.length; i++) {
+    const str = chord[i];
+    if (str.includes("#")) {
+      madeStave.addModifier(new Accidental("#"), i);
+    }
+  }
+  const notes = [madeStave];
 
   const voices = [
     new Voice({
@@ -49,7 +88,8 @@ function drawChord() {
   voices.forEach(function(v) {
     v.draw(context, stave);
   });
-  return pickKey[pickRoot][0]
+
+  return chord[0].split('/')[0];
 }
 
 /*****************************************/
